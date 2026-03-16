743efea5c1a8a7e669acff7d6509fac5bf4ab71b
Kaladhar Reddy Kethireddy(UST <246180@ust.com>
2026-03-16 18:45:17 +0530

    added new provider lines as per error

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
   version = "~> 2.54"
   region  = "us-east-1"
   access_key = "AKIAIOSFODNN7EXAMPLE"
@@ -10,7 +10,20 @@ provider "digitalocean" {}
 terraform {
     required_version = "0.12.31"
 }
+*/
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.54"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
 
 resource "aws_eip" "kplabs_app_ip" {
   vpc      = true
